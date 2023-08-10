1beeabe0c2767dc112c2d120c3ef7ccaf5ab60ed
Oleh <oleh.voitynskyi@devcom.com>
2023-08-10 23:39:57 +0300

    first day swift

--- 1day.swift
+++ 1day.swift
@@ -1,1 +1,38 @@
-print('hello world')
+// variables
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+print(favoriteShow)
+// string and integers
+var meaningOfLife = 42
+// Multi-line strings
+var quote = "Change the world by being yourself"
+var burns = """
+The best laid schemes
+O’ mice and men
+Gang aft agley
+"""
+print(burns)
+
+// doubles integers
+var myInt = 1
+var myDouble = 1.0
+
+//String interpolation
+var score = 85
+var str = "Your score was \(score)"
+print(str)
+
+var results = "The test results are here: \(str)"
+print(results)
+
+// constants
+let taylor = "swift"
+print(taylor)
+
+// type annotations
+
+var percentage: Double = 99
+var name: String = "Oleh"
+
+print("\(percentage) for \(name)")
